c6dd8f0e9d64743dd98d0f84ede720dff82bcb93
Jodexx <0984134q@gmail.com>
2024-05-27 12:11:17 +0300

    simply code, using timestamp

--- src/main/java/com/jodexindustries/freecases/events/EventListener.java
+++ src/main/java/com/jodexindustries/freecases/events/EventListener.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class EventListener implements Listener {
     private final FreeCases freeCases;
@@ -19,17 +18,7 @@ public class EventListener implements Listener {
         Player player = event.getPlayer();
             if (!freeCases.getAddonConfig().getDataFile().getStringList("Used").contains(player.getName()) ||
                     !freeCases.getAddonConfig().getConfig().getBoolean("GetOneTime")) {
-                CooldownManager.setCooldown(player.getUniqueId(), freeCases.getAddonConfig().getConfig().getInt("TimeToPlay"));
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        int timeLeft = CooldownManager.getCooldown(player.getUniqueId());
-                        CooldownManager.setCooldown(player.getUniqueId(), --timeLeft);
-                        if (timeLeft == 0) {
-                            this.cancel();
-                        }
-                    }
-                }.runTaskTimer(freeCases.getPlugin(), 20, 20);
+                CooldownManager.setCooldown(player.getUniqueId(), System.currentTimeMillis());
             }
     }
 }
