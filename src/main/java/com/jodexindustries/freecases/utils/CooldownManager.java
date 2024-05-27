b1fe5dcc06e658c026acbd6a785fb9a514efb2ae
Jodexx <0984134q@gmail.com>
2024-05-27 12:10:41 +0300

    added formatTime method

--- src/main/java/com/jodexindustries/freecases/utils/CooldownManager.java
+++ src/main/java/com/jodexindustries/freecases/utils/CooldownManager.java
@@ -5,9 +5,9 @@ import java.util.Map;
 import java.util.UUID;
 
 public class CooldownManager {
-    public static final Map<UUID, Integer> cooldowns = new HashMap<>();
+    public static final Map<UUID, Long> cooldowns = new HashMap<>();
 
-    public static void setCooldown(UUID player, int time){
+    public static void setCooldown(UUID player, long time){
         if(time < 1) {
             cooldowns.remove(player);
         } else {
@@ -15,7 +15,7 @@ public class CooldownManager {
         }
     }
 
-    public static int getCooldown(UUID player){
-        return cooldowns.getOrDefault(player, 0);
+    public static long getCooldown(UUID player){
+        return cooldowns.getOrDefault(player, 0L);
     }
 }
--- src/main/java/com/jodexindustries/freecases/utils/Utils.java
+++ src/main/java/com/jodexindustries/freecases/utils/Utils.java
@@ -5,6 +5,7 @@ import com.jodexindustries.freecases.events.EventListener;
 import com.jodexindustries.freecases.bootstrap.FreeCases;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 
 import java.io.File;
@@ -30,5 +31,22 @@ public class Utils {
     }
     public static String setPlaceholders(OfflinePlayer player, String text) {
         return plugin.getPlugin().getServer().getPluginManager().getPlugin("PlaceholderAPI") == null ? text : PlaceholderAPI.setPlaceholders(player, text);
+    }
+    public static String formatTime(long time) {
+        String secondChar = ChatColor.translateAlternateColorCodes('&', plugin.getAddonConfig().getConfig().getString("Second"));
+        String minuteChar = ChatColor.translateAlternateColorCodes('&', plugin.getAddonConfig().getConfig().getString("Minute"));
+        String hourChar = ChatColor.translateAlternateColorCodes('&', plugin.getAddonConfig().getConfig().getString("Hour"));
+        long hours = time / 3600;
+        long minutes = (time / 60) - hours * 60;
+        long seconds = time % 60 % 60;
+        String hour = hours + hourChar;
+        String minute = minutes + minuteChar;
+        String second = seconds + secondChar;
+        if (seconds == 0) second = "";
+        if (hours == 0) {
+            hour = "";
+            if (minutes == 0) minute = "";
+        }
+        return hour + minute + second;
     }
 }
